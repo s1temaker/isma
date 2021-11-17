9596496d158b6d5274bf869de9e482087311f4ab
s1temaker <info@s1temaker.ru>
2021-11-17 10:51:57 +0200

    Main Slider

--- assets/tpl/js/script.js
+++ assets/tpl/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-
+    $("#mainSlider .owl-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        loop: true,
+        dots: true,
+        dotsContainer: "#mainSlider .owl__dots .col-auto",
+    });
 });
